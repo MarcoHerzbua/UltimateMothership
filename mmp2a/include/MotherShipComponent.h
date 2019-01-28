316b894bd333a6aa38501aaad651e9ac0575f5fc
David Monz <dmonz.mmt-b2017@fh-salzburg.ac.at>
2019-01-28 17:07:37 +0100

    added Mothership

--- mmp2a/include/MotherShipComponent.h
+++ mmp2a/include/MotherShipComponent.h
@@ -5,5 +5,15 @@
 
 class MotherShipComponent : ShipComponent
 {
+public:
+	MotherShipComponent(GameObject* gameObject);
+	MotherShipComponent(GameObject* gameObject, NLTmxMapObject& mapObject);
 
+	void update(const float deltaTime) override;
+
+	void initBaseStats() override;
+
+	void initTmxData() override;
+
+protected:
 };
--- mmp2a/src/MotherShipComponent.cpp
+++ mmp2a/src/MotherShipComponent.cpp
@@ -1,3 +1,52 @@
 #include "stdafx.h"
 #include "MotherShipComponent.h"
 
+#include "BasicAttackAbilityComponent.h"
+#include "UltimateAbilityAttackComponent.h"
+#include "FarmAbilityComponent.h"
+
+MotherShipComponent::MotherShipComponent(GameObject * gameObject)
+	: ShipComponent(gameObject)
+{
+	m_id = MOTHER_SHIP_COMPONENT;
+}
+
+MotherShipComponent::MotherShipComponent(GameObject * gameObject, NLTmxMapObject & mapObject)
+	: ShipComponent(gameObject, mapObject)
+{
+	m_id = MOTHER_SHIP_COMPONENT;
+}
+
+void MotherShipComponent::update(const float deltaTime)
+{
+
+}
+
+void MotherShipComponent::initBaseStats()
+{
+	Stats baseStats;
+
+	baseStats.attack = 50;
+	baseStats.defense = 50;
+	baseStats.life = 100;
+
+	baseStats.movement = 5;
+
+	m_baseStats = baseStats;
+	m_bonusStats = Stats();
+
+	IAbilityComponent* basicAttack = new BasicAttackAbilityComponent();
+	IAbilityComponent* ultimateAttack = new UltimateAttackAbilityComponent();
+	IAbilityComponent* farm = new FarmAbilityComponent();
+
+	m_abilities[BASIC_ATTACK_ABILITY] = basicAttack;
+	m_abilities[ULTIMATE_ATTACK_ABILITY] = ultimateAttack;
+	m_abilities[FARM_ABILITY] = farm;
+}
+
+void MotherShipComponent::initTmxData()
+{
+	ShipComponent::initTmxData();
+
+	m_gameObject = nullptr;
+}
