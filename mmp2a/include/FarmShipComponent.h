218a28dfd00ed9b50a4055b10e2c067ee4dd051d
David Monz <dmonz.mmt-b2017@fh-salzburg.ac.at>
2019-01-28 17:11:06 +0100

    added farmship

--- mmp2a/include/FarmShipComponent.h
+++ mmp2a/include/FarmShipComponent.h
@@ -5,5 +5,15 @@
 
 class FarmShipComponent : ShipComponent
 {
+public:
+	FarmShipComponent(GameObject* gameObject);
+	FarmShipComponent(GameObject* gameObject, NLTmxMapObject& mapObject);
 
+	void update(const float deltaTime) override;
+
+	void initBaseStats() override;
+
+	void initTmxData() override;
+
+protected:
 };
