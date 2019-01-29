618ed137723253c6096a353d01fbdb41c3fc9995
David Monz <dmonz.mmt-b2017@fh-salzburg.ac.at>
2019-01-29 14:44:26 +0100

    removed old assets, created TestMap for ships, ...

--- mmp2a/assets/MenuSet.tsx
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6bb1551f662c104155815644b946c84849335c2a1ecd4a605523f7fcde31da2b
-size 250
--- mmp2a/assets/backgroundSet.tsx
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:85ac6d7852105064bf5784eac34c69291788949230c8d8dd6cf885f983996121
-size 269
