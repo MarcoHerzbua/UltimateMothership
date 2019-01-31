bf1c916e5b4b0308dcf32d68690ba8f2b39aaa69
David Monz <dmonz.mmt-b2017@fh-salzburg.ac.at>
2019-01-31 11:12:26 +0100

    implemented spriteswitcher in cursor to show if move is possible or not

--- mmp2a/src/CursorComponent.cpp
+++ mmp2a/src/CursorComponent.cpp
@@ -9,6 +9,7 @@
 #include "GameObjectManager.h"
 
 #include "ShipComponent.h"
+#include "SpriteSwitcherComponent.h"
 
 CursorComponent::CursorComponent(GameObject* gameObject)
 	: IGameComponent(gameObject)
@@ -35,10 +36,18 @@ void CursorComponent::updateCursor(const float deltaTimeSeconds)
 	
 	m_distanceToActive = graph->calcDistance(*(PlayerManager::getInstance().getActiveUnit()->getCurrentNode()), *(m_steering->getCurrentNode()));
 
-	
+	bool moveInRange = PlayerManager::getInstance().getActiveShip()->getCurrentMovement() >= m_distanceToActive;
+
+	auto switcher = static_cast<SpriteSwitcherComponent*>(m_gameObject->findComponents(SPRITE_SWITCHER_COMPONENT)[0]);
+
+	if (!moveInRange)
+		switcher->activateSet(1);
+	else
+		switcher->activateSet(0);
+
 	if (InputManager::getInstance().isKeyPressed(MOVE_ABILITY_ACTION, m_steering->getPlayerIndex()))
 	{
-		if (PlayerManager::getInstance().getActiveShip()->getCurrentMovement() >= m_distanceToActive)
+		if (moveInRange)
 		{
 			PlayerManager::getInstance().getActiveUnit()->moveToTargetNode(m_steering->getCurrentNode());
 			PlayerManager::getInstance().getActiveShip()->decreasMovement(m_distanceToActive);
